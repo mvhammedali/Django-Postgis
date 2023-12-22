b59351dccc3e69dfa30e7c65918787befcff25c4
Muhammed Ali <muhamzyali@gmail.com>
2023-12-22 09:51:56 +0100

    add app to settings

--- django_postgis/settings.py
+++ django_postgis/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # New apps
+    "mapper",
+    "auth"
+
 ]
 
 MIDDLEWARE = [
