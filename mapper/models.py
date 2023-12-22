f460c30ec328309c43b7e073233657be915bea61
Muhammed Ali <muhamzyali@gmail.com>
2023-12-22 10:19:30 +0100

    mapper model update

--- mapper/models.py
+++ mapper/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.gis.db import models
 
-# Create your models here.
+class PointFeature(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    geom = models.PointField()
+
+    def __str__(self):
+        return self.name
+
+class LineFeature(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    geom = models.LineStringField()
+
+    def __str__(self):
+        return self.name
+
+class PolygonFeature(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    geom = models.PolygonField()
+
+    def __str__(self):
+        return self.name
+
