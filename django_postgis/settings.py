2b381a283316b6a4c042b3bb1935556d853da340
Muhammed Ali <muhamzyali@gmail.com>
2024-03-26 15:03:54 +0100

    added postgis for settings

--- django_postgis/settings.py
+++ django_postgis/settings.py
@@ -79,12 +79,15 @@ WSGI_APPLICATION = "django_postgis.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'template',
+        'USER': 'postgres',
+        'PASSWORD': 'secure-password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
